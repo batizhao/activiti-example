2cbba672e2e013f62e265fa29be324931339543a
Bati Zhao <zhaobati@gmail.com>
2017-02-21 11:53:07 +0800

    git init.

---
+++ src/main/java/me/batizhao/ActivitiExampleApplication.java
@@ -0,0 +1,12 @@
+package me.batizhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiExampleApplication.class, args);
+	}
+}
